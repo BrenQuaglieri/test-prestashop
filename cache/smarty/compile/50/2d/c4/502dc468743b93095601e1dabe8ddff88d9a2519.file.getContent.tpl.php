4c0f9562ac66887265c0df770003d99fc7f692e1
BrenQuaglieri <BrendanQuaglieri@gmail.com>
2019-03-04 14:16:53 +0100

    Prestashop chapitre 1 / Module grades & comments

---
+++ cache/smarty/compile/50/2d/c4/502dc468743b93095601e1dabe8ddff88d9a2519.file.getContent.tpl.php
@@ -0,0 +1,71 @@
+<?php /* Smarty version Smarty-3.1.19, created on 2019-03-04 14:11:54
+         compiled from "/var/www/html/test-prestashop/modules/mymodcomments/views/templates/hook/getContent.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:11864565515c7d0c8891ce50-74755351%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '502dc468743b93095601e1dabe8ddff88d9a2519' => 
+    array (
+      0 => '/var/www/html/test-prestashop/modules/mymodcomments/views/templates/hook/getContent.tpl',
+      1 => 1551705106,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '11864565515c7d0c8891ce50-74755351',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_5c7d0c8891e623_77814555',
+  'variables' => 
+  array (
+    'confirmation' => 0,
+    'enable_grades' => 0,
+    'enable_comments' => 0,
+  ),
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5c7d0c8891e623_77814555')) {function content_5c7d0c8891e623_77814555($_smarty_tpl) {?><?php if (isset($_smarty_tpl->tpl_vars['confirmation']->value)) {?>
+<div class="alert alert-success">
+La configuration a bien été mise à jour
+</div>
+<?php }?>
+<form method="post" action="" class="defaultForm form-horizontal">
+    <div class="panel">
+        <div class="panel-heading">
+            <i class="icon-cogs"></i>
+            La configuration de mon module
+        </div>
+
+        <div class="form-wrapper">
+            <div class="form-group">
+                <label class="control-label col-lg-3">Activer les notes:</label>
+                <div class="col-lg-9">
+                    <img src="../img/admin/enabled.gif" alt="">
+                    <input type="radio" id="enable_grades_1" name="enable_grades" value="1" <?php if ($_smarty_tpl->tpl_vars['enable_grades']->value=='1') {?> checked <?php }?> />
+                    
+                    <label class="t" for="enable_grades_1">Oui</label>
+                    <img src="../img/admin/disabled.gif" alt="">
+                    <input type="radio" id="enable_grades_0" name="enable_grades" value="0" <?php if ($_smarty_tpl->tpl_vars['enable_grades']->value!='1') {?> checked <?php }?> />
+                    <label class="t" for="enable_grades_0">Non</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-lg-3">Activer les commentaires:</label>
+                <div class="col-lg-9">
+                    <img src="../img/admin/enabled.gif" alt="">
+                    <input type="radio" id="enable_comments_1" name="enable_comments" value="1" <?php if ($_smarty_tpl->tpl_vars['enable_comments']->value=='1') {?> checked <?php }?> />
+                    <label class="t" for="enable_comments_1">Oui</label>
+                    <img src="../img/admin/disabled.gif" alt="">
+                    <input type="radio" id="enable_comments_0" name="enable_comments" value="0" <?php if ($_smarty_tpl->tpl_vars['enable_comments']->value!='1') {?> checked <?php }?> />
+                    <label class="t" for="enable_comments_0">Non</label>
+                </div>
+            </div>
+        </div>
+        <div class="panel-footer">
+            <button class="btn btn-default pull-right" name="submit_mymodcomments_form" value="1" type="submit">
+                <i class="process-icon-save"></i> Enregistrer
+            </button>
+        </div>
+    </div>
+</form><?php }} ?>
--- modules/mymodcomments/mymodcomments.php
+++ modules/mymodcomments/mymodcomments.php
@@ -11,6 +11,35 @@ class MyModComments extends Module
          $this->displayName = 'Mon module de commentaires produits';
          $this->description = 'Avec ce module, vos clients pourront noter et commenter vos produits !';
 
+         $this->bootstrap = true;
+
          parent::__construct();
+     }
+
+     public function processConfiguration()
+     {
+        if (Tools::isSubmit('submit_mymodcomments_form'))
+        {
+            $enable_grades = Tools::getValue('enable_grades');
+            $enable_comments = Tools::getValue('enable_comments');
+            Configuration::updateValue('MYMOD_GRADES', $enable_grades);
+            Configuration::updateValue('MYMOD_COMMENTS', $enable_comments);
+            $this->context->smarty->assign('confirmation', 'ok');
+        }
+     }
+
+     public function assignConfiguration()
+     {
+         $enable_grades = Configuration::get('MYMOD_GRADES');
+         $enable_comments = Configuration::get('MYMOD_COMMENTS');
+         $this->context->smarty->assign('enable_grades', $enable_grades);
+         $this->context->smarty->assign('enable_comments', $enable_comments);
+    }
+
+     public function getContent()
+     {
+         $this->processConfiguration();
+         $this->assignConfiguration();
+         return $this->display(__FILE__, 'getContent.tpl');
      }
 }
